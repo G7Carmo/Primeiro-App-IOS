2ec3eb694139abf48a0fbb7768a68f09c133bb03
GabrielGDS <gcarmodev@gmail.com>
2022-03-27 03:50:47 -0300

    inclusao da table view dos igredientes

--- Controller/RefeicoesTableViewController.swift
+++ Controller/RefeicoesTableViewController.swift
@@ -8,16 +8,11 @@
 import Foundation
 import UIKit
 
-class RefeicoesTableViewController : UITableViewController{
-    
-    let refeicoes = [Refeicao(nome: "Macarrao", felicidade: 5),
+class RefeicoesTableViewController : UITableViewController,AdicionarRefeicaoDelegate{
+    var refeicoes = [Refeicao(nome: "Macarrao", felicidade: 5),
                      Refeicao(nome: "lasanha", felicidade: 4),
                      Refeicao(nome: "pizza", felicidade: 3),
-                     Refeicao(nome: "estrogonof", felicidade: 1),
-                     Refeicao(nome: "sushi", felicidade: 5),
-                     Refeicao(nome: "batata frita", felicidade: 2),
-                     Refeicao(nome: "feijoada", felicidade: 2),
-                     Refeicao(nome: "bolo de festa", felicidade: 5)]
+                     Refeicao(nome: "estrogonof", felicidade: 1),]
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return refeicoes.count
@@ -27,6 +22,18 @@ class RefeicoesTableViewController : UITableViewController{
         let refeicao = refeicoes[indexPath.row]
         celula.textLabel?.text = refeicao.nome
         return celula
+    }
+    
+    func add(_ refeicao: Refeicao){
+        refeicoes.append(refeicao)
+        tableView.reloadData()
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "adicionar"{
+            if let viewController = segue.destination as? ViewController{
+                viewController.delegate = self
+            }
+        }
     }
     
 }
--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -6,22 +6,48 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+protocol AdicionarRefeicaoDelegate{
+    func add(_ refeicao: Refeicao)
+}
+class ViewController: UIViewController ,UITableViewDataSource{
+    
+    //MAKR: - LISTA
+    let itens = [Itens(nome: "Queijo", calorias: 100.0),
+                 Itens(nome: "alface", calorias: 100.0),
+                 Itens(nome: "tomate", calorias: 100.0),
+                 Itens(nome: "chocolate", calorias: 100.0),
+                 Itens(nome: "feijao", calorias: 100.0)]
+    
+    //MARK: - TABLEVIEWACTIONS
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let item = itens[indexPath.row]
+        celula.textLabel?.text = item.nome
+        return celula
+        
+    }
+//MARK: - Atributos
+    var delegate : AdicionarRefeicaoDelegate?
+//    MARK: - IBOutlet
     @IBOutlet var nomeTextField : UITextField?
     @IBOutlet var felicidadeTExtView : UITextField?
+    
+//    MARK: - IBActions
     @IBAction func adicionar(){
-        if let nomeDaRefeicao = nomeTextField?.text,
-            let felicidadeDaRefeicao = felicidadeTExtView?.text {
-            let nome = nomeDaRefeicao
-            if let felicidade = Int(felicidadeDaRefeicao){
-                let refeicao = Refeicao(nome: nome, felicidade: felicidade)
-                print("alimento \(refeicao.nome) e fiquei com felicidade \(refeicao.felicidade)")
-            }else{
-                print("erro ao tentar criar refeicao")
-            }
-            
+        guard let nomeDaRefeicao = nomeTextField?.text else{
+            return
         }
+        guard let felicidadeDaRefeicao = felicidadeTExtView?.text,let felicidade  = Int(felicidadeDaRefeicao) else{
+            return
+        }
+        let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade)
+        print("alimento \(refeicao.nome) e fiquei com felicidade \(refeicao.felicidade)")
+        delegate?.add(refeicao)
+        navigationController?.popViewController(animated: true)
     }
 
 
